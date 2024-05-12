da334105819debe793b499f6feb6465c9285b750
GitHub-2021-07-12 <GitHub-2021-07-12@yandex.ru>
2024-05-12 20:37:57 +0300

    _

--- Main/Main.js
+++ Main/Main.js
@@ -1,2 +1,12 @@
-import '../Components/Root/Root.js';
+import {Root} from '../Components/Root/Root.js';
+// import {MessageFlow} from '../Components/MessageFlow/MessageFlow.js';
 // import '../ServiceWorker/Client.js';
+
+
+// class Main {
+//     static {
+//         MessageFlow.elements_classes = {
+//             display: ['Theme__block', 'Theme__button'],
+//         };
+//     }
+// }
